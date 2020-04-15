c0a201ef27673a27598a3693f8e3b8937460fc2d
Guilherme de Almeida <guilherme.ampp@gmail.com>
2020-04-14 23:01:56 -0300

    Challenge 4: Applying SOLID principles to our backend
    * Completed the /transaction routes
    * Completed the service class for the business rules
    * Completed the repository class for transactions
    * All tests passing!

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactions: number[] = [];
+    const outcomeTransactions: number[] = [];
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        incomeTransactions.push(transaction.value);
+      } else {
+        outcomeTransactions.push(transaction.value);
+      }
+    });
+
+    const totalIncome = incomeTransactions.reduce((a, b) => a + b, 0);
+    const totalOutcome = outcomeTransactions.reduce((a, b) => a + b, 0);
+    const balanceTotal = totalIncome - totalOutcome;
+
+    return {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: balanceTotal,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+    if (
+      type === 'outcome' &&
+      this.transactionsRepository.getBalance().total < value
+    ) {
+      throw Error(`Not enough balance to register an outcome of ${value}`);
+    }
+
+    const transaction = new Transaction({ title, value, type });
+    this.transactionsRepository.create(transaction);
+    return transaction;
   }
 }
 
